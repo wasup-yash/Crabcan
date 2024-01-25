588825a3cd91914dadfb64fb30fbf15722e8114b
wasup-yash <trooper0018080@gmail.com>
2024-01-25 23:55:35 +0530

    setting up for capabilities
    Signed-off-by: wasup-yash <trooper0018080@gmail.com>

--- crabcan/src/error.rs
+++ crabcan/src/error.rs
@@ -41,6 +41,7 @@ pub enum Ourerror {
     RngError,
     MountError(u8),
     NamespacesError(u8),
+    CapabilitiesError(u8),
 }
 impl Ourerror {
     //translate an error code::X into a number to return {the UNIX way}
